34597aec4104c4a84ce533fac106f5102c47b90d
iammridul503 <iammridul503.it@gmail.com>
2020-08-06 21:57:32 +0530

    Dagger Hilt setup

--- app/src/main/java/com/moriarity_code/trackyourfit/activity/MainActivity.kt
+++ app/src/main/java/com/moriarity_code/trackyourfit/activity/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.moriarity_code.trackyourfit.R
 
+
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/moriarity_code/trackyourfit/utility/Constants.kt
@@ -0,0 +1,6 @@
+package com.moriarity_code.trackyourfit.utility
+
+object Constants {
+
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
