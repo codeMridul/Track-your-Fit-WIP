2b7f819c53b65e30355c8d7b69cbe48e13f9fe61
codeMoriarity <iammridul503.it@gmail.com>
2020-09-12 20:21:28 +0530

    Background Services Updated

--- app/src/main/java/com/moriarity_code/trackyourfit/services/TrackingService.kt
+++ app/src/main/java/com/moriarity_code/trackyourfit/services/TrackingService.kt
@@ -7,23 +7,58 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import com.moriarity_code.trackyourfit.R
 import com.moriarity_code.trackyourfit.activity.MainActivity
 import com.moriarity_code.trackyourfit.utility.Constants.ACTION_PAUSE_SERVICE
 import com.moriarity_code.trackyourfit.utility.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.moriarity_code.trackyourfit.utility.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.moriarity_code.trackyourfit.utility.Constants.ACTION_STOP_SERVICE
+import com.moriarity_code.trackyourfit.utility.Constants.LOCATION_UPDATE_INTERVAL
 import com.moriarity_code.trackyourfit.utility.Constants.NOTIFICATION_CHANNEL_ID
 import com.moriarity_code.trackyourfit.utility.Constants.NOTIFICATION_CHANNEL_NAME
 import com.moriarity_code.trackyourfit.utility.Constants.NOTIFICATION_ID
+import com.moriarity_code.trackyourfit.utility.TrackingUtility
 import timber.log.Timber
 
 class TrackingService : LifecycleService() {
     var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<MutableList<MutableList<LatLng>>>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -47,7 +82,59 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackingUtility.checkLocationPermission(this)) {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = LOCATION_UPDATE_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+            if (isTracking.value!!) {
+                locationResult?.locations?.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("New Location: ${location.latitude}, ${location.longitude} ")
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService() {
+        addEmptyPolyline()
+        isTracking.postValue(true)
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
                 as NotificationManager
 
